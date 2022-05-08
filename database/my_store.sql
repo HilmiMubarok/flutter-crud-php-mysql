e53e643192f34b11c1ebce3af4e002329d02b816
HilmiMubarok <m.hilmimubarok@gmail.com>
2022-05-08 20:15:08 +0700

    add database

---
+++ database/my_store.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 08, 2022 at 03:14 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `my_store`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_item`
+--
+
+CREATE TABLE `tb_item` (
+  `id` int(11) NOT NULL,
+  `item_code` text NOT NULL,
+  `item_name` text NOT NULL,
+  `price` int(11) NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_item`
+--
+
+INSERT INTO `tb_item` (`id`, `item_code`, `item_name`, `price`, `stock`) VALUES
+(1, '123', 'Laptop', 200000, 20),
+(2, '234', 'HP', 1000000, 100);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_item`
+--
+ALTER TABLE `tb_item`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_item`
+--
+ALTER TABLE `tb_item`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
